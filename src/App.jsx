4629969bb1027e9fc133f0364b5cce13f330f48f
prithvi <prithvraj.bundela@gmail.com>
2025-04-15 15:53:56 +0530

    add changes P

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
-import Home from './Pages/home'
+// import Home from './Pages/home'
+import Home from './Pages/Home';
 import SignIn from './Pages/signin'
 import SignUp from './Pages/Signup'
 import Err from './Pages/Err';
