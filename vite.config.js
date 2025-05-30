7ab4fe24818deb93b8fcd5f0c23e1a9a25e9fbe9
ojasva_surawat <surawatojasva@gmail.com>
2025-05-30 23:54:03 +0530

    new file added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 import path from 'path' 
 
 export default defineConfig({
+  server: {
+    historyApiFallback: true, // This ensures that any non-file requests fallback to index.html
+  },
+  build: {
+    outDir: 'dist', // Ensure your build outputs to the 'dist' directory
+  },
+  base: '/', // This sets the base path for your app. Adjust if necessary.
   plugins: [
     react(),
     tailwindcss(),],
